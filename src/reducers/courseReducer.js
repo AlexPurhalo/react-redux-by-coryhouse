d3314b07b60a9b253c0029e60863c75be3cc4957
alexpurhalo <alexpurhalo@gmail.com>
2016-08-31 09:34:02 -0400

    form-for-course-manage-page

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -2,7 +2,7 @@ import * as types from '../actions/actionTypes';
 
 export default function courseReducer(state = [], action) {
 	switch(action.type) {
-		case types.LOAD_COURSE_SUCCESS:
+		case types.LOAD_COURSES_SUCCESS:
 			return action.courses;
 
 		default:
